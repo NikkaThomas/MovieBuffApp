ab40fa88967c8f41f10e8b2e961262489741b590
Anika <anika.tomichen@attinadsoftware.com>
2020-02-03 18:19:24 +0530

    movie app project created

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Anika Tomichen on 03/02/20.
+//  Copyright © 2020 Anika Tomichen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
